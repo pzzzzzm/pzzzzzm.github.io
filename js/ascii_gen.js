111f829b6d1789ffd4d8cfa497495330660acfd2
DeanBeanPudding <51908595+pzzzzzm@users.noreply.github.com>
2022-12-01 19:49:04 +0800

    add block based method

--- js/ascii_gen.js
+++ js/ascii_gen.js
@@ -1,4 +1,22 @@
 
+$('#dot-based').on('click', function (){
+    $('.type-block button').val(0).html('点深度二值化');
+    $('#thresh-value').removeAttr('disabled');
+    $('.thresh-comment').html('0-255');
+})
+
+$('#block-based').on('click', function (){
+    $('.type-block button').val(1).html('块深度');
+    $('#thresh-value').attr('disabled', 'disabled');
+    $('.thresh-comment').html('不可用');
+})
+
+$('#block-based-equalized').on('click', function (){
+    $('.type-block button').val(2).html('均衡化的块深度');
+    $('#thresh-value').attr('disabled', 'disabled');
+    $('.thresh-comment').html('不可用');
+})
+
 
 function getInput() {
     return cv.imread('input-img');
@@ -8,7 +26,7 @@ function processImgMat(mat, row) {
     let w = mat.size()['width'];
     let h = mat.size()['height'];
 
-    let w_char = 3*row;
+    let w_char = 3 * row;
     let h_char = Math.floor(h / w * w_char);
     let h_compl = 0;
 
@@ -62,6 +80,86 @@ function preprocessing(mat) {
     return convertGray(mat);
 }
 
+function dotBasedConvert(img_char, thresh) {
+    let w = img_char.size()['width'];
+    let h = img_char.size()['height'];
+
+    let res = '';
+
+    for (let r = 0; r < h; r += 5) {
+        for (let c = 0; c < w; c += 3) {
+            let sum = 10241;
+            // if (img_char.data[r * w + c] < thresh) sum += 1;
+            if (img_char.data[(r + 1) * w + c] < thresh) sum += 2;
+            if (img_char.data[(r + 2) * w + c] < thresh) sum += 4;
+            if (img_char.data[(r + 3) * w + c] < thresh) sum += 64;
+            if (img_char.data[r * w + c + 1] < thresh) sum += 8;
+            if (img_char.data[(r + 1) * w + c + 1] < thresh) sum += 16;
+            if (img_char.data[(r + 2) * w + c + 1] < thresh) sum += 32;
+            if (img_char.data[(r + 3) * w + c + 1] < thresh) sum += 128;
+            res = res + String.fromCharCode('0x' + sum.toString(16));
+        }
+        res = res + '<br/>';
+    }
+
+    return res;
+}
+
+function blockBasedConvert(img_char, is_equalized) {
+
+    let w = img_char.size()['width']/3;
+    let h = Math.floor(img_char.size()['height']/2.5);
+
+    let res = '';
+
+    let sample_size = new cv.Size(w, h);
+
+    cv.resize(img_char, img_char, sample_size, 0, 0, cv.INTER_AREA);
+    if (is_equalized) cv.equalizeHist(img_char, img_char);
+
+    for (let r = 0; r < h; r += 2) {
+        for (let c = 0; c < w; c += 1) {
+            let sum = 10240;
+            let depth1 = Math.floor(img_char.data[r*w+c]/32);
+            let depth2 = Math.floor(img_char.data[(r+1)*w+c]/32);
+
+            if (depth1 < 2) sum += 1;
+            if (depth1 < 4) sum += 16;
+            if (depth1 < 6) sum += 8;
+            if (depth1 < 8) sum += 2;
+
+            if (depth2 < 2) sum += 128;
+            if (depth2 < 4) sum += 4;
+            if (depth2 < 6) sum += 64;
+            if (depth2 < 8) sum += 32;
+
+            if (depth1 === 7 && depth2 === 7) sum = 10242;
+
+            // switch (depth) {
+            //     case 0:
+            //         sum += 64;
+            //     case 1:
+            //         sum += 8;
+            //     case 2:
+            //         sum += 32;
+            //     case 3:
+            //         sum += 2;
+            //     case 4:
+            //         sum += 128;
+            //     case 5:
+            //         sum += 1;
+            //     case 6:
+            //         sum += 16;
+            //     case 7:
+            //         sum += 4;
+            // }
+            res = res + String.fromCharCode('0x' + sum.toString(16));
+        }
+        res = res + '<br/>';
+    }
+    return res;
+}
+
 function convertImage() {
 
     // let row = 35;
@@ -69,6 +167,7 @@ function convertImage() {
 
     let row = $('#col-value').val();
     let thresh = $('#thresh-value').val();
+    let conv_type = $('.type-block button').val();
 
     let img = getInput();
     let img_g = preprocessing(img);
@@ -76,39 +175,16 @@ function convertImage() {
 
     let img_char = processImgMat(img_g, row);
     img_g.delete()
-    let w = img_char.size()['width'];
-    let h = img_char.size()['height'];
 
-    let res = '';
-    let roi_block = new cv.Mat();
+    let res;
 
-    // for (let r = 0; r < h; r += 4) {
-    //     for (let c = 0; c < w; c += 2) {
-    //         let rect = new cv.Rect(c, r, 2, 4);
-    //         roi_block = img_char.roi(rect);
-    //         res = res + block2Unicode(roi_block, thresh);
-    //     }
-    //     res = res + '\n';
-    // }
-
-    for (let r = 0; r < h; r += 5) {
-        for (let c = 0; c < w; c += 3) {
-            let sum = 10241;
-            // if (img_char.data[r * w + c] < thresh) sum += 1;
-            if (img_char.data[(r+1) * w + c] < thresh) sum += 2;
-            if (img_char.data[(r+2) * w + c] < thresh) sum += 4;
-            if (img_char.data[(r+3) * w + c] < thresh) sum += 64;
-            if (img_char.data[r * w + c + 1] < thresh) sum += 8;
-            if (img_char.data[(r+1) * w + c + 1] < thresh) sum += 16;
-            if (img_char.data[(r+2) * w + c + 1] < thresh) sum += 32;
-            if (img_char.data[(r+3) * w + c + 1] < thresh) sum += 128;
-            res = res + String.fromCharCode('0x' + sum.toString(16));
-        }
-        res = res + '<br/>';
+    switch (conv_type){
+        case '0': res = dotBasedConvert(img_char, thresh); break;
+        case '1': res = blockBasedConvert(img_char, 0); break;
+        case '2': res = blockBasedConvert(img_char, 1); break;
     }
 
-    // img_char.delete();
-    roi_block.delete();
+    img_char.delete();
 
     // return img_char;
     // $('#output-text textarea').text(res).attr('rows', h).attr('cols', w);
